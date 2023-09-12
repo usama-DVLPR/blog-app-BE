dd8ae7505356335435ae68971e53025adf8701ea
Usama Hameed <113657499+usama-DVLPR@users.noreply.github.com>
2023-09-13 02:15:23 +0500

    remove unnecessary code

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI, {});
+    await mongoose.connect(process.env.DB_URI);
     console.log("Database is connected");
   } catch (error) {
     console.log(`Error 💥 ${error.message}`);
