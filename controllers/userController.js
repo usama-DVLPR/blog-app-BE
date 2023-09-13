8d6ae56eb28a00a9e2980685ab784414d4dd15ad
Usama Hameed <113657499+usama-DVLPR@users.noreply.github.com>
2023-09-13 08:18:58 +0500

    register user api

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+import User from "../models/User";
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // check user exists or not
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already registered" });
+    }
+    // creating new user
+    user = await User.create({ name, email, password });
+    res.status(201).json({
+      _id: user._id,
+      avatar: user.avatar,
+      name: user.name,
+      email: user.email,
+      verified: user.verified,
+      admin: user.admin,
+      token: null,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+
+import userRoutes from "./routes/userRoutes";
+
 dotenv.config();
 
 connectDB();
@@ -10,6 +13,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("server is running");
 });
+app.use("/api/users", userRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
