fb49b6c01f3a13846a870644a4ae5591308b7c5a
Usama Hameed <113657499+usama-DVLPR@users.noreply.github.com>
2023-09-16 11:39:12 +0500

    auth middleware completed

---
+++ controllers/authMiddleware.js
@@ -0,0 +1,25 @@
+import { verify } from "jsonwebtoken";
+import User from "../models/User";
+
+export const authGuard = async (req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      console.log(token);
+      const { id } = verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(id).select("-password");
+      next();
+    } catch (error) {
+      let err = new Error("Not authorized, Token faild");
+      err.statusCode = 401;
+      next(err);
+    }
+  } else {
+    let error = new Error("Not authorized,not token");
+    error.statusCode = 401;
+    next(error);
+  }
+};
